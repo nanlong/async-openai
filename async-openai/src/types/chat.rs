2f5fa3dc998caaa10f0445b39b5ae9176f7ae7bd
Jeff <fei.code@gmail.com>
2024-08-17 01:05:38 +0800

    chore: update CreateChatCompletionStreamResponse type

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -649,14 +649,14 @@ pub struct CreateChatCompletionStreamResponse {
     pub choices: Vec<ChatChoiceStream>,
 
     /// The Unix timestamp (in seconds) of when the chat completion was created. Each chunk has the same timestamp.
-    pub created: u32,
+    pub created: Option<u32>,
     /// The model to generate the completion.
-    pub model: String,
+    pub model: Option<String>,
     /// This fingerprint represents the backend configuration that the model runs with.
     /// Can be used in conjunction with the `seed` request parameter to understand when backend changes have been made that might impact determinism.
     pub system_fingerprint: Option<String>,
     /// The object type, which is always `chat.completion.chunk`.
-    pub object: String,
+    pub object: Option<String>,
 
     /// An optional field that will only be present when you set `stream_options: {"include_usage": true}` in your request.
     /// When present, it contains a null value except for the last chunk which contains the token usage statistics for the entire request.
